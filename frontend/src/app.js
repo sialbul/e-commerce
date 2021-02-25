afdd2c6d6accf7bfb19337999f6ed3990813f979
Sibel <sbaslamisli@yahoo.com>
2021-02-24 21:33:49 -0500

    node.js server created

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -2,7 +2,7 @@ import Error404Screen from './screens/Error404Screen.js';
 import HomeScreen from './screens/HomeScreen.js';
 import ProductScreen from './screens/ProductScreen.js';
 import { parseRequestUrl } from './utils.js';
-const routes = {
+const routes = { //routes a oject and "/" is key and "HomeScreen" is a value
     "/": HomeScreen,
     "/product/:id": ProductScreen,
 }
