c59f0605681b56cdb5777e64ecdba2ae2bedb121
Sibel <sbaslamisli@yahoo.com>
2021-02-26 00:14:05 -0500

    to get Product details

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,5 +1,14 @@
+import { getProduct } from '../api';
+import { parseRequestUrl } from '../utils';
+
 const ProductScreen = {
-    render: () => `
-        <div>ProductScreen</div>`,
+    render: async() => {
+        const request = parseRequestUrl();
+        const product = await getProduct(request.id);
+        if (product.error) {
+            return (`<div>${product.error}</div>`);
+        }
+        return `<h1>${product.name}</h1>`;
+    }
 };
 export default ProductScreen;
