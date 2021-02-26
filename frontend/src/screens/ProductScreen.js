ed7b357f9fe3a57b76e85904b934d0b00f95926a
Sibel <sbaslamisli@yahoo.com>
2021-02-26 02:18:32 -0500

    to show Product details  on the details page

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,14 +1,70 @@
 import { getProduct } from '../api';
 import { parseRequestUrl } from '../utils';
+import Rating from '../components/Rating';
 
 const ProductScreen = {
-    render: async() => {
-        const request = parseRequestUrl();
-        const product = await getProduct(request.id);
-        if (product.error) {
-            return (`<div>${product.error}</div>`);
-        }
-        return `<h1>${product.name}</h1>`;
+        render: async() => {
+                const request = parseRequestUrl();
+                const product = await getProduct(request.id);
+                if (product.error) {
+                    return (`<div>${product.error}</div>`);
+                }
+                return `
+        <div class="content">
+            <div class="back-to-result">
+                <a href="/#">Back to result </a>
+            </div>
+            <div class="details">
+                <div class="details-image">
+                    <img src="${product.image}" alt="${product.name}" />
+                </div>
+                <div class="details-info">
+                    <ul>
+                        <li>
+                            <h1>${product.name}</h1>
+                        </li>
+                        <li>
+                            ${Rating.render({
+            value: product.rating,
+            text: `${product.numReviews} reviews`
+        })}
+                        </li>
+                        <li>
+                        Price: <strong>${product.price}</strong>
+                        </li>
+                        <li>
+                        Description:
+                            <div>
+                            ${product.description}
+                            </div>
+
+                        </li>
+
+                    </ul>
+                </div>     
+                <div class="details-action">
+                <ul>
+                <li>
+                    Price:${product.price}
+
+                </li>
+                <li>
+                    Status :
+                        ${product.countInStock > 0
+                ? `<span class="success">In Stock</span>` :
+                `<span class="error">Unavailable</span>`
+            }
+                </li>
+                <li>
+<button id="add-button" class=" fw primary">Add to Cart </div>
+                </li>
+                </ul>
+
+                </div> 
+        </div>
+        
+        
+        `;
     }
 };
 export default ProductScreen;
