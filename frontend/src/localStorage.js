8f1a80039c0cf6ef8934eb94532694caae64dfb6
Sibel <sbaslamisli@yahoo.com>
2021-02-28 00:37:38 -0500

    add product to cart

---
+++ frontend/src/localStorage.js
@@ -0,0 +1,5 @@
+export const getCartItems = () => {
+    const cartItems = localStorage.getItem('cartItems') ?
+        JSON.parse(localStorage.getItem('cartItems')) : [];
+    return cartItems;
+}
--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,5 +1,26 @@
+import { getProduct } from "../api";
+import { parseRequestUrl } from "../utils";
+const addToCart = (item, forceUpDate = false) => {
+    let cartItems = getCartItems();
+
+};
 const CartScreen = {
     after_render: () => {},
-    render: () => `<div> Cart Screen </div>`,
+    render: async() => {
+        const request = parseRequestUrl;
+        if (request.id) {
+            const product = await getProduct(request.id);
+            addToCart({
+                product: product._id,
+                name: product.name,
+                image: product.image,
+                price: product.price,
+                countInStock: product.countInStock,
+                qty: 1,
+            })
+        }
+
+        return `<div> Cart Screen </div>`,
+    }
 }
 export default CartScreen;
