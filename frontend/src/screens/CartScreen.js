7b2ecaee6776416a50b5ffc7a335bce9578aba50
Sibel <sbaslamisli@yahoo.com>
2021-02-28 06:59:48 -0500

    cart section-cart UI

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -15,23 +15,63 @@ const addToCart = (item, forceUpDate = false) => {
     setCartItems(cartItems);
 };
 const CartScreen = {
-    after_render: () => {},
-    render: async() => {
-        const request = parseRequestUrl();
-        if (request.id) {
-            const product = await getProduct(request.id);
-            addToCart({
-                product: product._id,
-                name: product.name,
-                image: product.image,
-                price: product.price,
-                countInStock: product.countInStock,
-                qty: 1,
-            })
-        }
+        after_render: () => {},
+        render: async() => {
+                const request = parseRequestUrl();
+                if (request.id) {
+                    const product = await getProduct(request.id);
+                    addToCart({
+                        product: product._id,
+                        name: product.name,
+                        image: product.image,
+                        price: product.price,
+                        countInStock: product.countInStock,
+                        qty: 1,
+                    })
+                }
+                const cartItems = getCartItems();
+                return `
+        <div class="cart">
+        <div class="cart-list">
+            <ul class="cart-list-container">
+                <li> 
+                    <h3> Shopping Cart </h3>
+                    <div>Price</div>
+                </li>
+                ${cartItems.length === 0 ? '<div>Cart is empth.<a href="/#/">Go Shopping</a></div>' :
+                cartItems.map(item => `
+                <li>
+                    <div class="cart-image">
+                        <img src="${item.image}" alt="${item.name}"/>
+                    </div>
+                    <div class="cart-name">
+                        <div>
+                            <a href="/#/product/${item.product}">
+                                ${item.name}
+                            </a>
+                        </div>
+                        <div>
+                             Qty: 
+                            <select class="qty-select" id="${item.product}">
+                                <option value="1">1</option>
+                            </select>
+                            <button type="button" class="delete-button" id="${item.product}">Delete</button>
+                        </div>
+                    </div>
+                    <div class="cart-price">
+                        $${item.price}
+                    </div>
+                </li>
+                `).join('\n')
+            }
+            </ul>
+        </div>
+        <div class="cart-list">
+        </div>
 
-        return `<div> Cart Screen </div>
-        <div>${getCartItems().length}</div>
+        
+        
+        </div>
         `;
     },
 };
