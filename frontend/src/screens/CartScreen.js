3973a79ecc1c61db460d7825c5e1fb02f59cf356
Sibel <sbaslamisli@yahoo.com>
2021-03-03 01:44:18 -0500

    sass setup

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,14 +1,13 @@
 /* eslint-disable no-use-before-define */
+import { parseRequestUrl, rerender } from '../utils';
+import { getProduct } from '../api';
+import { getCartItems, setCartItems } from '../localStorage';
 
-import { getProduct } from "../api";
-import { parseRequestUrl, rerender } from "../utils";
-import { getCartItems, setCartItems } from "../localStorage";
-
-const addToCart = (item, forceUpDate = false) => {
+const addToCart = (item, forceUpdate = false) => {
     let cartItems = getCartItems();
-    const existItem = cartItems.find(x => x.product === item.product);
+    const existItem = cartItems.find((x) => x.product === item.product);
     if (existItem) {
-        if (forceUpDate) {
+        if (forceUpdate) {
             cartItems = cartItems.map((x) =>
                 x.product === existItem.product ? item : x
             );
@@ -17,11 +16,10 @@ const addToCart = (item, forceUpDate = false) => {
         cartItems = [...cartItems, item];
     }
     setCartItems(cartItems);
-    if (forceUpDate) {
+    if (forceUpdate) {
         rerender(CartScreen);
     }
 };
-
 const removeFromCart = (id) => {
     setCartItems(getCartItems().filter((x) => x.product !== id));
     if (id === parseRequestUrl().id) {
@@ -29,25 +27,26 @@ const removeFromCart = (id) => {
     } else {
         rerender(CartScreen);
     }
-}
+};
+
 const CartScreen = {
         after_render: () => {
-            const qtySelects = document.getElementsByClassName("qty-select");
+            const qtySelects = document.getElementsByClassName('qty-select');
             Array.from(qtySelects).forEach((qtySelect) => {
                 qtySelect.addEventListener('change', (e) => {
                     const item = getCartItems().find((x) => x.product === qtySelect.id);
                     addToCart({...item, qty: Number(e.target.value) }, true);
-
                 });
-            })
-            const deleteButtons = document.getElementsByClassName("delete-button");
+            });
+            const deleteButtons = document.getElementsByClassName('delete-button');
             Array.from(deleteButtons).forEach((deleteButton) => {
                 deleteButton.addEventListener('click', () => {
                     removeFromCart(deleteButton.id);
-
                 });
-            })
-
+            });
+            document.getElementById('checkout-button').addEventListener('click', () => {
+                document.location.hash = '/signin';
+            });
         },
         render: async() => {
                 const request = parseRequestUrl();
@@ -60,60 +59,71 @@ const CartScreen = {
                         price: product.price,
                         countInStock: product.countInStock,
                         qty: 1,
-                    })
+                    });
                 }
                 const cartItems = getCartItems();
                 return `
-        <div class="content cart">
-        <div class="cart-list">
-            <ul class="cart-list-container">
-                <li> 
-                    <h3> Shopping Cart </h3>
-                    <div>Price</div>
-                </li>
-                ${cartItems.length === 0 ? '<div>Cart is empty.<a href="/#/">Go Shopping</a></div>' :
-                cartItems.map(item => `
-                <li>
-                    <div class="cart-image">
-                        <img src="${item.image}" alt="${item.name}"/>
-                    </div>
-                    <div class="cart-name">
-                        <div>
-                            <a href="/#/product/${item.product}">
-                                ${item.name}
-                            </a>
-                        </div>
-                        <div>
-                             Qty: 
-                            <select class="qty-select" id="${item.product}">
-${[...Array(item.countInStock).keys()].map((x) => item.qty === x + 1 ?
-                    `<option selected value="${x + 1}">${x + 1}</option>` :
-                    `<option value="${x + 1}">${x + 1}</option>`
-                )}
-                    </select>
-                            <button type="button" class="delete-button" id="${item.product}">Delete</button>
-                        </div>
-                    </div>
-                    <div class="cart-price">
-                        $${item.price}
-                    </div>
-                </li>
-                `).join('\n')
-            }
-            </ul>
-        </div>
-            <div class="cart-action">
-            <h3>
-                Subtotal (${cartItems.reduce((a, c) => a + c.qty, 0)} items)
-                :
-                $${cartItems.reduce((a, c) => a + c.price * c.qty, 0)}
-            </h3>
-            <button id="checkout-button" class="primary fw">
+    <div class="content cart">
+      <div class="cart-list">
+        <ul class="cart-list-container">
+          <li>
+            <h3>Shopping Cart</h3>
+            <div>Price</div>
+          </li>
+          ${cartItems.length === 0
+                ? '<div>Cart is empty. <a href="/#/">Go Shopping</a>'
+                : cartItems
+                    .map(
+                        (item) => `
+            <li>
+              <div class="cart-image">
+                <img src="${item.image}" alt="${item.name}" />
+              </div>
+              <div class="cart-name">
+                <div>
+                  <a href="/#/product/${item.product}">
+                    ${item.name}
+                  </a>
+                </div>
+                <div>
+                  Qty: 
+                  <select class="qty-select" id="${item.product}">
+                  ${[...Array(item.countInStock).keys()].map((x) =>
+                            item.qty === x + 1
+                                ? `<option selected value="${x + 1}">${x + 1}</option>`
+                                : `<option  value="${x + 1}">${x + 1}</option>`
+                        )}  
+
+                  </select>
+                  <button type="button" class="delete-button" id="${item.product
+                            }">
+                    Delete
+                  </button>
+                </div>
+              </div>
+              <div class="cart-price">
+                $${item.price}
+              </div>
+            </li>
+            `
+                    )
+                    .join('\n')
+            } 
+        </ul>
+      </div>
+      <div class="cart-action">
+          <h3>
+            Subtotal (${cartItems.reduce((a, c) => a + c.qty, 0)} items)
+            :
+            $${cartItems.reduce((a, c) => a + c.price * c.qty, 0)}
+          </h3>
+          <button id="checkout-button" class="primary fw">
             Proceed to Checkout
-            </button>
-        </div>
-        </div>
-        `;
+          </button>
+      </div>
+    </div>
+    `;
     },
 };
+
 export default CartScreen;
