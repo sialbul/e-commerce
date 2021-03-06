cd47b78df78e43dd44a6b26b5f5d9acfa4b5f575
Sibel <sbaslamisli@yahoo.com>
2021-03-06 00:39:46 -0500

    styling front page

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -11,7 +11,51 @@ const HomeScreen = {
             </div>
         </div>
     </section>
-    
+    <div class="features-container">
+    <section class="features">
+      <h2>Features</h2>
+      <div class="cards">
+        <div class="card">
+          <img src="./images/product-10.jpg" alt="" />
+          <h4>Black Shoes</h4>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
+        </div>
+        <div class="card">
+        <img src="./images/product-12.jpg" alt="" />
+        <h4>Man Joggers</h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit
+          </p>
+        </div>
+        <div class="card">
+        <img src="./images/product-11.jpg" alt="" />
+        <h4>White Shoes</h4>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
+        </div>
+     
+      </div>
+    </section>
+    <img class="wave" src="./images/wave.png" alt="" />
+  </div><section class="soon">
+  <h2>Coming Soon</h2>
+  <img src="./images/product-12.jpg" alt="" />
+  <img class="circle" src="./images/circle.png" alt="" />
+</section>
+<footer>
+  <h1>Leica 2020</h1>
+  <ul>
+    <li>
+      <a href="#"><img src="./images/yt-logo.png" alt="" /></a>
+    </li>
+    <li>
+      <a href="#"><img src="./images/twitter-logo.png" alt="" /></a>
+    </li>
+    <li>
+      <a href="#"><img src="./images/instagram-logo.png" alt="" /></a>
+    </li>
+  </ul>
+</footer>
+
     `,
 
 };
